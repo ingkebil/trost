fcba05ccfa36b50cc14c2ef49e9f23a8b9cc3d3a
ingkebil <kennybilliau@gmail.com>
2011-05-16 16:10:22 +0000

    Fixed the truncate problem; See http://bugs.mysql.com/bug.php?id=58788 for more explanation
    git-svn-id: file:///home/kebil/svnadmin/trost/trunk@35 c6f7d11c-5fb9-4030-b9f5-87cb8ea52402

--- scripts/truncate.pl
+++ scripts/truncate.pl
@@ -12,11 +12,21 @@ my $pass = $ARGV[1] || &usage;
 my $db   = $ARGV[2] || 'trost';
 my $host = $ARGV[2] || 'localhost';
 
-`echo 'TRUNCATE phenotype_bbches;'   | mysql -u $user -p$pass -h $host $db`;
-`echo 'TRUNCATE phenotype_entities;' | mysql -u $user -p$pass -h $host $db`;
-`echo 'TRUNCATE phenotype_raws;'     | mysql -u $user -p$pass -h $host $db`;
-`echo 'TRUNCATE phenotype_values;'   | mysql -u $user -p$pass -h $host $db`;
-`echo 'TRUNCATE raws;'               | mysql -u $user -p$pass -h $host $db`;
-`echo 'TRUNCATE temps;'              | mysql -u $user -p$pass -h $host $db`;
-`echo 'TRUNCATE phenotypes;'         | mysql -u $user -p$pass -h $host $db`;
-`echo 'TRUNCATE plants;'             | mysql -u $user -p$pass -h $host $db`;
+#`echo 'TRUNCATE phenotype_bbches;'   | mysql -u $user -p$pass -h $host $db`;
+#`echo 'TRUNCATE phenotype_entities;' | mysql -u $user -p$pass -h $host $db`;
+#`echo 'TRUNCATE phenotype_raws;'     | mysql -u $user -p$pass -h $host $db`;
+#`echo 'TRUNCATE phenotype_values;'   | mysql -u $user -p$pass -h $host $db`;
+#`echo 'TRUNCATE raws;'               | mysql -u $user -p$pass -h $host $db`;
+#`echo 'TRUNCATE temps;'              | mysql -u $user -p$pass -h $host $db`;
+#`echo 'TRUNCATE phenotypes;'         | mysql -u $user -p$pass -h $host $db`;
+#`echo 'TRUNCATE plants;'             | mysql -u $user -p$pass -h $host $db`;
+
+
+`echo 'DELETE FROM phenotype_bbches;'   | mysql -u $user -p$pass -h $host $db`;
+`echo 'DELETE FROM phenotype_entities;' | mysql -u $user -p$pass -h $host $db`;
+`echo 'DELETE FROM phenotype_raws;'     | mysql -u $user -p$pass -h $host $db`;
+`echo 'DELETE FROM phenotype_values;'   | mysql -u $user -p$pass -h $host $db`;
+`echo 'DELETE FROM raws;'               | mysql -u $user -p$pass -h $host $db`;
+`echo 'DELETE FROM temps;'              | mysql -u $user -p$pass -h $host $db`;
+`echo 'DELETE FROM phenotypes;'         | mysql -u $user -p$pass -h $host $db`;
+`echo 'DELETE FROM plants;'             | mysql -u $user -p$pass -h $host $db`;
