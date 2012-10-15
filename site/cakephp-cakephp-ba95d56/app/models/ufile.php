e9050a72f7c57eae41a9f5c1d42d8c73efb014f4
ingkebil <kennybilliau@gmail.com>
2012-10-15 15:38:32 +0000

    Removed an invaled validation rule that would block invalidating files
    git-svn-id: file:///home/kebil/svnadmin/trost/trunk@131 c6f7d11c-5fb9-4030-b9f5-87cb8ea52402

--- site/cakephp-cakephp-ba95d56/app/models/ufile.php
+++ site/cakephp-cakephp-ba95d56/app/models/ufile.php
@@ -3,16 +3,16 @@ class ufile extends AppModel {
 
 	var $name = 'ufile';
 	var $validate = array(
-		'submitter' => array(
-			'notempty' => array(
-				'rule' => array('notempty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
+		#'submitter' => array(
+		#	'notempty' => array(
+		#		'rule' => array('notempty'),
+		#		//'message' => 'Your custom message here',
+		#		//'allowEmpty' => false,
+		#		//'required' => false,
+		#		//'last' => false, // Stop validation after this rule
+		#		//'on' => 'create', // Limit validation to 'create' or 'update' operations
+		#	),
+		#),
 		'name' => array(
 			'notempty' => array(
 				'rule' => array('notempty'),
