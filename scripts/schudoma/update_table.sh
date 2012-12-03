c27072deefea419f77a58b6c9c751a18ea963621
ingkebil <kennybilliau@gmail.com>
2012-12-03 13:42:51 +0000

    Merged the update_*.sh scripts and added redundancy check
    git-svn-id: file:///home/kebil/svnadmin/trost/trunk@154 c6f7d11c-5fb9-4030-b9f5-87cb8ea52402

---
+++ scripts/schudoma/update_table.sh
@@ -0,0 +1,35 @@
+table=$1
+data_dir=~/svn/trost/trunk/data/backups
+backup_file=$data_dir/trost_prod_`date +%F_%X`.sql.gz
+plants_file=$data_dir/${table}_`date +%F_%X`.sql
+
+# get the previous $table file
+prev_plants_file=(`ls -t $data_dir/$table_*`)
+prev_plants_file=$prev_plants_file[1]
+echo "Found old $table: $prev_plants_file"
+
+# generate the new plants
+echo -n "Generating new $table ..."
+echo "/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;" > $plants_file
+python update_$table.py >> $plants_file
+echo "/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;" >> $plants_file
+echo "done: $plants_file"
+
+# compare new plants with old plants
+diff=`diff -q $prev_plants_file $plants_file`
+if [[ $? -eq 0 || $? -eq 1 ]]; then
+    if [[ -n $diff ]]; then
+        echo -n "Making backup ..."
+        mysqldump -u backup -ppasswordpassw -h cosmos --default-character-set=latin1 --skip-set-charset trost_prod | pigz -9 > $backup_file
+        echo "done."
+        echo -n "Updating $table ..."
+        mysql -u billiau -ppassword -h cosmos trost_prod < $plants_file
+        echo "done."
+    else
+        echo -n "No changes found, removing $plants_file ..."
+        `rm $plants_file`
+        echo "done."
+    fi
+else
+    echo "ERR: $diff"
+fi
