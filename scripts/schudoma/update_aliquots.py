d6895ad0e3157c50f89c9f36c9a199bf39924662
ingkebil <kennybilliau@gmail.com>
2013-03-05 16:52:01 +0000

    Had to redesign how the aliquots connect to the plants table, so this is the script to fill it from LIMS
    git-svn-id: file:///home/kebil/svnadmin/trost/trunk@170 c6f7d11c-5fb9-4030-b9f5-87cb8ea52402

--- scripts/schudoma/update_aliquots.py
+++ scripts/schudoma/update_aliquots.py
@@ -8,26 +8,44 @@ DB_NAME = 'trost_prod'
 TABLE_NAME = 'aliquots'
 """
 
-def format(*params):
-    return "REPLACE into `aliquots` VALUES (%s, %s, %s, %s, %s, %s);" % params
+def format(aliquot):
+
+    rs = """
+    INSERT INTO `aliquots` (id, sample_date, amount, amount_unit, organ)
+    VALUES (%s, %s, %s, %s, %s)
+    ON DUPLICATE KEY UPDATE
+    sample_date=VALUES(sample_date),
+    amount=VALUES(amount),
+    amount_unit=VALUES(amount_unit),
+    organ=VALUES(organ);
+    """ % (
+        aliquot['ALIQUOT'],
+        aliquot['U_SAMPLED_ON'],
+        aliquot['AMOUNT'],
+        aliquot['UNIT_ID'],
+        aliquot['ORGAN']
+    )
+
+    rs += """
+    INSERT INTO `aliquot_plants` (aliquot_id, plant_id)
+    VALUES (%s, %s)
+    ON DUPLICATE KEY UPDATE
+    aliquot_id=VALUES(aliquot_id),
+    plant_id=VALUES(plant_id);
+    """ % (
+        aliquot['ALIQUOT'],
+        aliquot['PLANT']
+    )
+
+    return rs
 
 def main(argv):
 
     aliquots = ora_sql.get_all_aliquots_info()
 
+    print "/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;"
     for aliquot in aliquots:
-        print format(
-            aliquot['ALIQUOT'],
-            aliquot['PLANT'],
-            aliquot['U_SAMPLED_ON'],
-            aliquot['AMOUNT'],
-            aliquot['UNIT_ID'],
-            aliquot['ORGAN']
-        )
+        print format(aliquot)
+    print "/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;"
 
 if __name__ == '__main__': main(sys.argv[1:])
-
-
-"""
-
-"""
